fdefcf8bb7cff2cfd5721a3883ca36a16977a67e
Mike Segers <M.Segers-1@student.tudelft.nl>
2022-05-02 10:49:36 +0200

    Adding template

--- src/sensors/MagneticDeclinationPublisher.js
+++ src/sensors/MagneticDeclinationPublisher.js
@@ -1,121 +1,141 @@
- let compass;
- 
- function isIOS() {
-    return [
-      'iPad Simulator',
-      'iPhone Simulator',
-      'iPod Simulator',
-      'iPad',
-      'iPhone',
-      'iPod'
-    ].includes(navigator.platform)
-    // iPad on iOS 13 detection
-    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+//Dependencies
+var Quaternion = require('quaternion');
+var ROSLIB = require('roslib');
+const SensorPublisher = require('./SensorPublisher.js');
+
+let compass;
+let pointDegree;
+
+const isIOS = !(
+  navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+  navigator.userAgent.match(/AppleWebKit/)
+);
+
+ class MagneticDeclinationPublisher extends SensorPublisher {
+  /**
+   * Creates a new sensor publisher that publishes to the provided topic.
+   * @param {Topic} topic a Topic from RosLibJS
+   */
+  constructor(topic) {
+    super(topic);
+    var self = this;
+    this.topic = topic;
+    this.freq = 0.5;
+
+    // First need to detect first device orientation.
+    this.orientationReady = false;
+    this.motionReady = false;
+
+    if (isIOS) {
+      DeviceOrientationEvent.requestPermission()
+        .then((response) => {
+          if (response === "granted") {
+            window.addEventListener('deviceorientation', (event) => {
+              this.onReadOrientation(self, event);
+            });  
+            
+            // start sensor
+            this.start();
+            console.log('started!');
+          } else {
+            alert("has to be allowed!");
+          }
+        })
+        .catch(() => alert("not supported"));
+    } else {
+      window.addEventListener('deviceorientation', (event) => {
+        this.onReadOrientation(self, event);
+      });
+
+      // start sensor
+      this.start();
+      console.log('started!');
+    }
   }
 
-  let pointDegree;
-
-function locationHandler(position) {
-  const { latitude, longitude } = position.coords;
-  pointDegree = calcDegreeToPoint(latitude, longitude);
-
-  if (pointDegree < 0) {
-    pointDegree = pointDegree + 360;
+  /**
+   * Callback for when error occurs while reading sensor data.
+   * @param {*} event containing error info.
+   */
+  onError(event) {
+      throw 'ERROR!';
   }
-}
 
-function calcDegreeToPoint(latitude, longitude) {
-  // Qibla geolocation
-  const point = {
-    lat: 21.422487,
-    lng: 39.826206,
-  };
-
-  const phiK = (point.lat * Math.PI) / 180.0;
-  const lambdaK = (point.lng * Math.PI) / 180.0;
-  const phi = (latitude * Math.PI) / 180.0;
-  const lambda = (longitude * Math.PI) / 180.0;
-  const psi =
-    (180.0 / Math.PI) *
-    Math.atan2(
-      Math.sin(lambdaK - lambda),
-      Math.cos(phi) * Math.tan(phiK) -
-        Math.sin(phi) * Math.cos(lambdaK - lambda)
-    );
-  return Math.round(psi);
-}
-
- class MagneticDeclinationPublisher {
-    /**
-     * Creates a new sensor publisher that publishes to the provided topic.
-     * @param {Topic} topic a Topic from RosLibJS
-     */
-    constructor(topic) {
-        this.topic = topic;
-        navigator.geolocation.getCurrentPosition(locationHandler);
-    }
-
-    /**
-     * Callback for when error occurs while reading sensor data.
-     * @param {*} event containing error info.
-     */
-    onError(event) {
-        throw 'ERROR';
-    }
-
-    /**
-     * Callback for reading sensor data.
-     * Should publish data to ROS topic.
+  /**
+     * Callback for reading orientation data.
+     * @param {IMU} self context of object that called callback.
      * @param {*} event object containing sensor data.
      */
-    onReadData(event) {
-        compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
-        compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
+   onReadOrientation(self, event) {
+    self.alpha = event.alpha;
+    self.beta = event.beta;
+    self.gamma = event.gamma;
+    self.orientationReady = true;
 
-        // ±15 degree
-        if (
-            (pointDegree < Math.abs(compass) && pointDegree + 15 > Math.abs(compass)) ||
-            pointDegree > Math.abs(compass + 15) ||
-            pointDegree < Math.abs(compass)
-        ) {
-            myPoint.style.opacity = 0;
-        } else if (pointDegree) {
-            myPoint.style.opacity = 1;
-        }
-    }
+    navigator.geolocation.getCurrentPosition(locationHandler);
+  }
 
-    /**
-     * Start the publishing of data to ROS.
-     */
-    start() {
-        if (isIOS) {
-            DeviceOrientationEvent.requestPermission()
-              .then((response) => {
-                if (response === "granted") {
-                  window.addEventListener("deviceorientation", handler, true);
-                } else {
-                  alert("has to be allowed!");
-                }
-              })
-              .catch(() => alert("not supported"));
-          } else {
-            window.addEventListener("deviceorientationabsolute", handler, true);
-          }
+  locationHandler(position) {
+    const { latitude, longitude } = position.coords;
+    pointDegree = calcDegreeToPoint(latitude, longitude);
+  
+    if (pointDegree < 0) {
+      pointDegree = pointDegree + 360;
     }
+  }
 
-    /**
-     * Stops the publishing of data to ROS.
-     */
-    stop() {
-        throw 'stop method not defined!';
-    }
+  calcDegreeToPoint(latitude, longitude) {
+    // Qibla geolocation
+    const point = {
+      lat: 21.422487,
+      lng: 39.826206,
+    };
+  
+    const phiK = (point.lat * Math.PI) / 180.0;
+    const lambdaK = (point.lng * Math.PI) / 180.0;
+    const phi = (latitude * Math.PI) / 180.0;
+    const lambda = (longitude * Math.PI) / 180.0;
+    const psi =
+      (180.0 / Math.PI) *
+      Math.atan2(
+        Math.sin(lambdaK - lambda),
+        Math.cos(phi) * Math.tan(phiK) -
+          Math.sin(phi) * Math.cos(lambdaK - lambda)
+      );
+    return Math.round(psi);
+  }
 
-    /**
-     * Sets the maximum frequency at which new data can be published.
-     */
-    setPublishFrequency() {
-        throw 'setPublishFrequency method not defined!';
-    }
+  createSnapshot() {
+    console.log('**SNAP**');
+    compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+
+    //pointDegree == richting??
+  }
+  
+
+  /**
+   * Start the publishing of data to ROS.
+   */
+  start() {
+    var delay = 1000/this.freq;
+    this.timer = setInterval(() => {
+        this.createSnapshot();
+    }, delay);
+  }
+
+  /**
+   * Stops the publishing of data to ROS.
+   */
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  /**
+   * Sets the maximum frequency at which new data can be published.
+   */
+  setPublishFrequency() {
+    this.freq = hz;
+  }
 }
 
 module.exports = MagneticDeclinationPublisher;
