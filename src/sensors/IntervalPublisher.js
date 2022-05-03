ac60ea2a7111df3f39e1b6dfe13a88cecaafc7af
Gissa <G.vandeLinde@student.tudelft.nl>
2022-05-03 15:30:29 +0200

    Update CheckStyle complaints

--- src/sensors/IntervalPublisher.js
+++ src/sensors/IntervalPublisher.js
@@ -1,52 +1,57 @@
 // Assumptions:
-    // A non-set timer is no problem.
+// A non-set timer is no problem.
 
 const SensorPublisher = require('./SensorPublisher');
- 
-class IntervalPublisher extends SensorPublisher {
 
-    /**
-     * Creates a new sensor publisher that publishes to the provided topic with a Regular interval.
+/**
+ * Interface-like class that can be extended by sensors that need
+ * their messages to be published at regular intervals.
+ */
+class IntervalPublisher extends SensorPublisher {
+  /**
+     * Creates a new sensor publisher that publishes to
+     * the provided topic with a Regular interval.
      * @param {Topic} topic a Topic from RosLibJS
      */
-    constructor(topic) {
-        // Super should have topic verification! @pcarton
-        super(topic);
-    }    
+  constructor(topic) {
+    // Super should have topic verification! @pcarton
+    super(topic);
+  }
 
-    /**
-     * Captures sensor-data at current timeframe and publishes this to the topic instantly. 
+  /**
+     * Captures sensor-data at current timeframe and
+     * publishes this to the topic instantly.
      */
-    createSnapshot() {
-        throw 'create snapshot is yet to be implemented!';
-    }
+  createSnapshot() {
+    throw Error('create snapshot is yet to be implemented!');
+  }
 
-    /**
+  /**
      * Start the publishing of data to ROS with frequency of <freq> Hz.
      */
-    start() {
-        var delay = 1000/this.freq;
-        let snapshotCallback = this.createSnapshot.bind(this);
-        this.timer = setInterval(snapshotCallback, delay);
-    }
+  start() {
+    const delay = 1000/this.freq;
+    const snapshotCallback = this.createSnapshot.bind(this);
+    this.timer = setInterval(snapshotCallback, delay);
+  }
 
-    /**
+  /**
      * Stops the publishing of data to ROS.
      */
-    stop() {
-        clearInterval(this.timer);
-    }
+  stop() {
+    clearInterval(this.timer);
+  }
 
-    /**
-     * Sets the maximum frequency at which new data can be published.
-     */
-    setPublishFrequency(hz) {
-        this.freq = hz;
-        // Restart timer with new frequency 
-        this.stop();
-        this.start();
-    }
-
+  /**
+  * Sets the maximum frequency at which new data can be published.
+  * @param {Int32} hz frequency to be used.
+  */
+  setPublishFrequency(hz) {
+    this.freq = hz;
+    // Restart timer with new frequency
+    this.stop();
+    this.start();
+  }
 }
 
 module.exports = IntervalPublisher;
--- test/unit/sensor/IntervalPublisherTest.js
+++ test/unit/sensor/IntervalPublisherTest.js
@@ -1,147 +1,154 @@
-var assert = require('assert')
+const assert = require('assert');
 const sinon = require('sinon');
 
 // JSDOM for simulating browser environment
-const { JSDOM } = require('jsdom');
-const { window } = new JSDOM(``, {});
+const {JSDOM} = require('jsdom');
+const {window} = new JSDOM(``, {});
 
 const IntervalPublisher = require('../../../src/sensors/IntervalPublisher.js');
 
 
-// define JSDOM window in global scope 
+// define JSDOM window in global scope
 global.window = global.window || window;
-const { document } = global.window;
+// const {document} = global.window;
 
-// create dummy for ROS 
+// create dummy for ROS
 global.ROSLIB = {
-    Topic: function() {
-        this.publish = function(msg) {}
-    },
-    Message: function(msg) {
-        this.msg = msg
-    }
-}
+  Topic: function() {
+    this.publish = function(msg) {};
+  },
+  Message: function(msg) {
+    this.msg = msg;
+  },
+};
 
-
+/**
+ * Helper method to create a new Interval Publisher implementation.
+ * @return {IntervalPublisher} a publisher object.
+ */
 function createIntervalPublisher() {
-    const topic = new ROSLIB.Topic('boo!');    
-    const IVPublisher = new IntervalPublisher(topic);
-    // Mock createSnapshot out
-    IVPublisher.createSnapshot = sinon.spy();
-    return IVPublisher;
+  const topic = new ROSLIB.Topic('boo!');
+  const IVPublisher = new IntervalPublisher(topic);
+  // Mock createSnapshot out
+  IVPublisher.createSnapshot = sinon.spy();
+  return IVPublisher;
 }
 
-describe('Test IntervalPublisher', function () {
+describe('Test IntervalPublisher', function() {
+  // setPublishFrequency tests
+  describe('#setPublishFrequency(hz)', function() {
+    it('Frequency of 1 Hz works correctly', function() {
+      // Arrange
+      const clock = sinon.useFakeTimers();
+      // Setup IMU object
+      const IVPublisher = createIntervalPublisher();
+      IVPublisher.createSnapshot = sinon.spy();
 
-   // setPublishFrequency tests
-    describe('#setPublishFrequency(hz)', function() {
-       
-        it('Frequency of 1 Hz works correctly', function () {     
-            // Arrange
-            var clock = sinon.useFakeTimers();
-            // Setup IMU object 
-            const IVPublisher = createIntervalPublisher();
-            IVPublisher.createSnapshot = sinon.spy();
+      // Act and Assert
+      IVPublisher.setPublishFrequency(1 /* Hz*/ );
+      clock.tick(200);
 
-            // Act and Assert
-            IVPublisher.setPublishFrequency(1 /*Hz*/ );
-            clock.tick(200); 
-            assert.equal(IVPublisher.createSnapshot.callCount, 0);     // After 0.2 seconds no publishment should be invoked yet
+      // After 0.2 seconds no publishment should be invoked yet
+      assert.equal(IVPublisher.createSnapshot.callCount, 0);
 
-            clock.tick(800);
-            assert.equal(IVPublisher.createSnapshot.callCount, 1);     // After exactly 1.0 seconds a single publishmunt is due.
+      clock.tick(800);
+      // After exactly 1.0 seconds a single publishmunt is due.
+      assert.equal(IVPublisher.createSnapshot.callCount, 1);
 
-            clock.tick(10 * 1000);                          // Wait for 10 seconds.
-            assert.equal(IVPublisher.createSnapshot.callCount, 11);    // After exactly 11.0 seconds 11 snapshots should be created. 
-            
-            clock.restore();
-        });
+      clock.tick(10 * 1000); // Wait for 10 seconds.
+      // After exactly 11.0 seconds 11 snapshots should be created.
+      assert.equal(IVPublisher.createSnapshot.callCount, 11);
 
-
-        it('Should succesfully be able to change frequency during publishing', function() {
-            // Arrange
-            var clock = sinon.useFakeTimers();
-            const IVPublisher = createIntervalPublisher();
-            IVPublisher.createSnapshot = sinon.spy();
- 
-            // Act
-            IVPublisher.setPublishFrequency(10);                    // Update 10 times a second.
-            clock.tick(1000);                               // Forward a second.
-            assert.equal(IVPublisher.createSnapshot.callCount, 10);    // Should call createSnapshot 10 times.
-
-            IVPublisher.setPublishFrequency(100);                   // Change frequency to 100 times a second.
-            clock.tick(1000);                               // Forward a second.
-            assert.equal(IVPublisher.createSnapshot.callCount, 110);   // Should call createSnapshot another 100 times.
-
-            clock.restore();
-        });
-
-        it('Should have the publisher restart at change of frequency', function() {
-            // Arrange
-            const IVPublisher = createIntervalPublisher();
-            IVPublisher.start = sinon.spy();
-            IVPublisher.stop = sinon.spy();
-
-            // Act
-            IVPublisher.setPublishFrequency(10);            
-            IVPublisher.setPublishFrequency(100);            
-            IVPublisher.setPublishFrequency(1);            
-
-            // Assert
-            assert.equal(IVPublisher.stop.callCount, 3);
-            assert.equal(IVPublisher.start.callCount, 3);
-        });
+      clock.restore();
     });
 
-    // start tests
-    describe('#start()', function() {
-        it('should start a timer', function() {
-            // Arrange
-            const IVPublisher = createIntervalPublisher();
-            const intervalSpy = sinon.spy(global, 'setInterval'); 
-            // Act
-            IVPublisher.start();  
-            // Assert
-            assert.equal(intervalSpy.callCount, 1);
-        });
+
+    it('Should be able to change frequency during publishing', function() {
+      // Arrange
+      const clock = sinon.useFakeTimers();
+      const IVPublisher = createIntervalPublisher();
+      IVPublisher.createSnapshot = sinon.spy();
+
+      // Act
+      IVPublisher.setPublishFrequency(10); // Update 10 times a second.
+      clock.tick(1000); // Forward a second.
+      // Should call createSnapshot 10 times.
+      assert.equal(IVPublisher.createSnapshot.callCount, 10);
+
+      // Change frequency to 100 times a second.
+      IVPublisher.setPublishFrequency(100);
+      clock.tick(1000); // Forward a second.
+
+      // Should call createSnapshot another 100 times.
+      assert.equal(IVPublisher.createSnapshot.callCount, 110);
+      clock.restore();
     });
 
-    // stop tests
-    describe('#stop()', function() {
-        const sandbox = sinon.createSandbox();
+    it('Should have the publisher restart at change of frequency', function() {
+      // Arrange
+      const IVPublisher = createIntervalPublisher();
+      IVPublisher.start = sinon.spy();
+      IVPublisher.stop = sinon.spy();
 
-        beforeEach(function () {
-            sandbox.spy(global, 'clearInterval');
-        });
+      // Act
+      IVPublisher.setPublishFrequency(10);
+      IVPublisher.setPublishFrequency(100);
+      IVPublisher.setPublishFrequency(1);
 
-        afterEach(function () {
-            sandbox.restore();
-        });
-
-        it('should stop a timer', function() {
-            // Arrange
-            const IVPublisher = createIntervalPublisher();
-            IVPublisher.start();  
-
-            // Act
-            IVPublisher.stop();
-
-            // Assert
-            assert.equal(global.clearInterval.callCount, 1);
-        });
-
-        it('should accept a non-existent timer', function() {
-            // Arrange
-            const IVPublisher = createIntervalPublisher();
-            IVPublisher.timer = undefined;
-
-            // Act
-            IVPublisher.stop();
-
-            // Assert
-            assert.equal(global.clearInterval.callCount, 1);
-        });
- 
+      // Assert
+      assert.equal(IVPublisher.stop.callCount, 3);
+      assert.equal(IVPublisher.start.callCount, 3);
     });
+  });
+
+  // start tests
+  describe('#start()', function() {
+    it('should start a timer', function() {
+      // Arrange
+      const IVPublisher = createIntervalPublisher();
+      const intervalSpy = sinon.spy(global, 'setInterval');
+      // Act
+      IVPublisher.start();
+      // Assert
+      assert.equal(intervalSpy.callCount, 1);
+    });
+  });
+
+  // stop tests
+  describe('#stop()', function() {
+    const sandbox = sinon.createSandbox();
+
+    beforeEach(function() {
+      sandbox.spy(global, 'clearInterval');
+    });
+
+    afterEach(function() {
+      sandbox.restore();
+    });
+
+    it('should stop a timer', function() {
+      // Arrange
+      const IVPublisher = createIntervalPublisher();
+      IVPublisher.start();
+
+      // Act
+      IVPublisher.stop();
+
+      // Assert
+      assert.equal(global.clearInterval.callCount, 1);
+    });
+
+    it('should accept a non-existent timer', function() {
+      // Arrange
+      const IVPublisher = createIntervalPublisher();
+      IVPublisher.timer = undefined;
+
+      // Act
+      IVPublisher.stop();
+
+      // Assert
+      assert.equal(global.clearInterval.callCount, 1);
+    });
+  });
 });
 
