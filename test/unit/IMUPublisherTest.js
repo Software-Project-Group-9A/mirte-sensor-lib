89bb7bd2721856e513a79a40df4d2c3440b3ae51
Gissa <G.vandeLinde@student.tudelft.nl>
2022-05-03 11:46:54 +0200

    Add quite some tests and make assertions trackable with equal

--- test/unit/IMUPublisherTest.js
+++ test/unit/IMUPublisherTest.js
@@ -19,13 +19,28 @@ global.ROSLIB = {
 }
 
 const IMUPublisher = require('../../src/sensors/IMUPublisher.js');
+const { I } = require('quaternion');
+
+/**
+ * Helper method to quickly get a working IMU going. 
+ * @returns a new IMU that is initialized and has true flags for indicating it has read some data. 
+ */
+function createStandardIMU() {
+    const topic = new ROSLIB.Topic("boo!"); 
+    // Setup IMU object 
+    const IMU = new IMUPublisher(topic);
+    IMU.orientationReady = true;
+    IMU.motionReady = true;
+    return IMU;
+}
+
 
 describe("Test IMU Publisher", function() {
-
-
+    
     // Constructor Tests
     describe('#constructor(topic)', function() {
-        it('Browser has device motion support', function() {
+
+        it('works when browser has device motion support', function() {
             // Arrange
             window.addEventListener = sinon.spy();
             window.DeviceMotionEvent = true;    
@@ -35,10 +50,10 @@ describe("Test IMU Publisher", function() {
             const IMU = new IMUPublisher(topic);
 
             // Assert
-            assert(window.addEventListener.callCount === 2);
+            assert.equal(window.addEventListener.callCount, 2);
         });
 
-        it('Browser doesn\'t have device motion support', function() {
+        it('works when browser doesn\'t have device motion support', function() {
             // Arrange
             window.addEventListener = sinon.spy();
             window.alert = sinon.spy();
@@ -49,14 +64,8 @@ describe("Test IMU Publisher", function() {
             const IMU = new IMUPublisher(topic);
 
            // Arrange 
-            assert(window.addEventListener.callCount === 1);
-            assert(window.alert.callCount === 1);
-        });
-    });
-
-    // Start tests
-    describe('#start()', function() {
-        it('starts', function() { 
+            assert.equal(window.addEventListener.callCount, 1);
+            assert.equal(window.alert.callCount, 1);
         });
     });
 
@@ -66,23 +75,20 @@ describe("Test IMU Publisher", function() {
         it('Frequency of 1 Hz works correctly', function () {     
             // Arrange
             var clock = sinon.useFakeTimers();
-            const topic = new ROSLIB.Topic("boo!"); 
             // Setup IMU object 
-            const IMU = new IMUPublisher(topic);
+            const IMU = createStandardIMU();
             IMU.createSnapshot = sinon.spy();
-            IMU.orientationReady = true;
-            IMU.motionReady = true;
 
-            // Act
+            // Act and Assert
             IMU.setPublishFrequency(1 /*Hz*/ );
             clock.tick(200); 
-            assert(IMU.createSnapshot.callCount === 0);     // After 0.2 seconds no publishment should be invoked yet
+            assert.equal(IMU.createSnapshot.callCount, 0);     // After 0.2 seconds no publishment should be invoked yet
 
             clock.tick(800);
-            assert(IMU.createSnapshot.callCount === 1);     // After exactly 1.0 seconds a single publishmunt is due.
+            assert.equal(IMU.createSnapshot.callCount, 1);     // After exactly 1.0 seconds a single publishmunt is due.
 
             clock.tick(10 * 1000);                          // Wait for 10 seconds.
-            assert(IMU.createSnapshot.callCount === 11);    // After exactly 11.0 seconds 11 snapshots should be created. 
+            assert.equal(IMU.createSnapshot.callCount, 11);    // After exactly 11.0 seconds 11 snapshots should be created. 
             
             clock.restore();
         });
@@ -91,27 +97,95 @@ describe("Test IMU Publisher", function() {
         it('Should succesfully be able to change frequency during publishing', function() {
             // Arrange
             var clock = sinon.useFakeTimers();
-            const topic = new ROSLIB.Topic("boo!"); 
-            // Setup IMU object 
-            const IMU = new IMUPublisher(topic);
+            const IMU = createStandardIMU();
             IMU.createSnapshot = sinon.spy();
-            IMU.orientationReady = true;
-            IMU.motionReady = true;
-
-
+ 
             // Act
             IMU.setPublishFrequency(10);                    // Update 10 times a second.
             clock.tick(1000);                               // Forward a second.
-            assert(IMU.createSnapshot.callCount === 10);    // Should call createSnapshot 10 times.
+            assert.equal(IMU.createSnapshot.callCount, 10);    // Should call createSnapshot 10 times.
 
             IMU.setPublishFrequency(100);                   // Change frequency to 100 times a second.
             clock.tick(1000);                               // Forward a second.
-            assert(IMU.createSnapshot.callCount === 110);   // Should call createSnapshot another 100 times.
+            assert.equal(IMU.createSnapshot.callCount, 110);   // Should call createSnapshot another 100 times.
 
             clock.restore();
-        })
+        });
+
+        it('Should have the publisher restart at change of frequency', function() {
+            // Arrange
+            const IMU = createStandardIMU();
+            IMU.start = sinon.spy();
+            IMU.stop = sinon.spy();
+
+            // Act
+            IMU.setPublishFrequency(10);            
+            IMU.setPublishFrequency(100);            
+            IMU.setPublishFrequency(1);            
+
+            // Assert
+            assert.equal(IMU.stop.callCount, 3);
+            assert.equal(IMU.start.callCount, 3);
+        });
     });
 
-    // tests
-    
+    // start tests
+    describe('#start()', function() {
+        it('should start a timer', function() {
+            // Arrange
+            // Setup IMU object 
+            const IMU = createStandardIMU();
+            const intervalSpy = sinon.spy(global, 'setInterval'); 
+            // Act
+            IMU.start();  
+            // Assert
+            assert.equal(intervalSpy.callCount, 1);
+        });
+    });
+
+    // stop tests
+    describe('#stop()', function() {
+        const sandbox = sinon.createSandbox();
+
+        beforeEach(function () {
+            sandbox.spy(global, 'clearInterval');
+        });
+
+        afterEach(function () {
+            sandbox.restore();
+        });
+
+        it('should stop a timer', function() {
+            // Arrange
+            const IMU = createStandardIMU();
+            IMU.start();  
+
+            // Act
+            IMU.stop();
+
+            // Assert
+            assert.equal(global.clearInterval.callCount, 1);
+        });
+
+        it('should accept a non-existent timer', function() {
+            // Arrange
+            const IMU = createStandardIMU();
+            IMU.timer = undefined;
+
+            // Act
+            IMU.stop();
+
+            // Assert
+            assert.equal(global.clearInterval.callCount, 1);
+        });
+ 
+    });
+
+
+    // createSnapshot tests
+    describe('#createSnapshot()', function() {
+        it('throws an exception when not both flags are true');
+        it('publishes to the topic.');
+        it('does quarternions correctly.');
+    });
 });
