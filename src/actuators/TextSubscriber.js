53ee2fc1e119d5bbeebe4adc5378b062f7b214bf
Tijs Lenssen <tijslenssen@icloud.com>
2022-05-09 12:09:08 +0200

    Added TextSubscriber that can display text published to a rostopic

---
+++ src/actuators/TextSubscriber.js
@@ -0,0 +1,38 @@
+const Subscriber = require('./Subscriber.js');
+
+/**
+ * TextSubscriber subscribes to a ROS topic and displays the received messages
+ * in an HTML element.
+ *
+ * The data should be from a topic with message type
+ * ROS std_msgs/String message.
+ */
+class TextSubscriber extends Subscriber {
+  /**
+   * Creates a new TextSubscriber.
+   * @param {ROSLIB.Topic} topic topic to which to publish text data
+   * @param {HTMLElement} HTMLElement HTML element in which the messages will be displayed.
+   */
+  constructor(topic, HTMLElement) {
+    super(topic);
+
+    if (!(HTMLElement instanceof window.HTMLElement)) {
+      throw new TypeError('input element was not of type HTMLElement');
+    }
+
+    this.HTMLElement = HTMLElement;
+  }
+
+  /**
+   * Callback that gets called when a message is received.
+   * Displays received message in HTML.
+   * @param {ROSLIB.Message} msg the received message
+   */
+  onMessage(msg) {
+    console.log(msg);
+    console.log(this);
+    this.HTMLElement.innerHTML = msg.data;
+  }
+}
+
+module.exports = TextSubscriber;
