64a1d85edc877608af3bd452c45933e799e40a1a
Gissa <G.vandeLinde@student.tudelft.nl>
2022-06-02 16:25:12 +0200

    copy three.js code to mathUtils

---
+++ src/mathUtils.js
@@ -0,0 +1,54 @@
+/**
+ * Convert Euler angle notation rotation to Quaternion notation.
+ * @param {number} x the Euler x coordinate
+ * @param {number} y the Euler angle y coordinate
+ * @param {number} z as Euler angle z coordinate
+ * @return {*} object containing Quaternian coordinates.
+ */
+function quatFromEuler(x, y, z) {
+  // This code was copied and modified from Three.js/src/math/Quaternion.js
+  // The modification of this code is allowed by the MIT license claimed by the Three.js project.
+
+  // http://www.mathworks.com/matlabcentral/fileexchange/
+  // 20696-function-to-convert-between-dcm-euler-angles-quaternions-and-euler-vectors/
+  // content/SpinCalc.m
+
+  const cos = Math.cos;
+  const sin = Math.sin;
+
+  const c1 = cos( x / 2 );
+  const c2 = cos( y / 2 );
+  const c3 = cos( z / 2 );
+
+  const s1 = sin( x / 2 );
+  const s2 = sin( y / 2 );
+  const s3 = sin( z / 2 );
+
+
+  const _x = s1 * c2 * c3 + c1 * s2 * s3;
+  const _y = c1 * s2 * c3 - s1 * c2 * s3;
+  const _z = c1 * c2 * s3 + s1 * s2 * c3;
+  const _w = c1 * c2 * c3 - s1 * s2 * s3;
+
+  return {'x': _x, 'y': _y, 'z': _z, 'w': _w};
+}
+
+/**
+ * info
+ * @param {*} x info
+ * @param {*} y info
+ * @param {*} z info
+ * @param {*} w info
+ * @return {*} aaa
+ */
+function eulerFromQuat(x, y, z, w) {
+  // This code was copied and modified from Three.js/src/math/Euler.js
+  // The modification of this code is allowed by the MIT license claimed by the Three.js project.
+
+
+  return x;
+}
+
+module.exports = {
+  quatFromEuler: quatFromEuler,
+};
--- src/sensors/IMUPublisher.js
+++ src/sensors/IMUPublisher.js
@@ -6,8 +6,43 @@
 // Use on IOS with requesting permission
 
 // Dependencies
-const THREE = require('three');
 const IntervalPublisher = require('./IntervalPublisher.js');
+
+/**
+ * Convert Euler angle notation rotation to Quaternion notation.
+ * @param {number} x the Euler x coordinate
+ * @param {number} y the Euler angle y coordinate
+ * @param {number} z as Euler angle z coordinate
+ * @return {*} object containing Quaternian coordinates.
+ */
+function quatFromEuler(x, y, z) {
+  // This code was copied and modified from Three.js/src/math/Quaternion.js
+  // The modification of this code is allowed by the MIT license claimed by the Three.js project.
+
+  // http://www.mathworks.com/matlabcentral/fileexchange/
+  // 20696-function-to-convert-between-dcm-euler-angles-quaternions-and-euler-vectors/
+  // content/SpinCalc.m
+
+  const cos = Math.cos;
+  const sin = Math.sin;
+
+  const c1 = cos( x / 2 );
+  const c2 = cos( y / 2 );
+  const c3 = cos( z / 2 );
+
+  const s1 = sin( x / 2 );
+  const s2 = sin( y / 2 );
+  const s3 = sin( z / 2 );
+
+
+  const _x = s1 * c2 * c3 + c1 * s2 * s3;
+  const _y = c1 * s2 * c3 - s1 * c2 * s3;
+  const _z = c1 * c2 * s3 + s1 * s2 * c3;
+  const _w = c1 * c2 * c3 - s1 * s2 * s3;
+
+  return {'x': _x, 'y': _y, 'z': _z, 'w': _w};
+}
+
 
 /**
  * Object that publishes IMU sensor data to the provided ROS topic.
@@ -98,11 +133,9 @@ class IMUPublisher extends IntervalPublisher {
     const alphaRad = ((this.alpha + 360) / 360 * 2 * Math.PI) % (2 * Math.PI);
     const betaRad = ((this.beta + 360) / 360 * 2 * Math.PI) % (2 * Math.PI);
     const gammaRad = ((this.gamma + 360) / 360 * 2 * Math.PI) % (2 * Math.PI);
-    const eurlerpose = new THREE.Euler(betaRad, gammaRad, alphaRad);
 
     // Create Quaternion based on device orientation
-    const q = new THREE.Quaternion();
-    q.setFromEuler(eurlerpose);
+    const q = quatFromEuler(betaRad, gammaRad, alphaRad);
 
     // Create imuMessage in ROS's IMU-message format.
     // For definition of message type see following source:
@@ -140,6 +173,9 @@ class IMUPublisher extends IntervalPublisher {
     // Publish message on designated topic.
     this.topic.publish(imuMessage);
   }
+
+
+
 }
 
 module.exports = IMUPublisher;
