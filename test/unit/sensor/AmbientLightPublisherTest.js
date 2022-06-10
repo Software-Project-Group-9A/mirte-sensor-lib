665e7fafb8a3dc7602838a572dbf1bfc53b0b9d2
Gissa <G.vandeLinde@student.tudelft.nl>
2022-06-10 10:13:41 +0200

    Add tests for AmbientLightPublisher

--- test/unit/sensor/AmbientLightPublisherTest.js
+++ test/unit/sensor/AmbientLightPublisherTest.js
@@ -4,7 +4,24 @@ require('../../globalSetup.js');
 const AmbientLightPublisher =
     require('../../../src/sensors/AmbientLightPublisher.js');
 
+// Helper functions
+/**
+ * creates spy sensor.
+ * @return {AmbientLightSensor} ambient light sensor.
+ */
+function spyOnLightSensor() {
+  global.window.AmbientLightSensor = true;
+  const mockSensor = {};
+  mockSensor.addEventListener = sinon.spy();
+  mockSensor.removeEventListener = sinon.spy();
+  mockSensor.start = sinon.spy();
+  mockSensor.stop = sinon.spy();
+  return mockSensor;
+}
+
+// Test
 describe('Test AmbientLightPublisher', function() {
+  // Test constructor
   describe('#constructor(topic)', function() {
     it('should fail when there is no API support', function() {
       global.window.AmbientLightSensor = false;
@@ -19,6 +36,58 @@ describe('Test AmbientLightPublisher', function() {
             new AmbientLightPublisher(new ROSLIB.Ros(), 'topic');
           }
       );
+    });
+  });
+  // Test start function
+  describe('#start()', function() {
+    it('should start with an event listener', function() {
+      // Arrange
+      const ros = new ROSLIB.Ros();
+      const publisher = new AmbientLightPublisher(ros, 'topic');
+      publisher.sensor = spyOnLightSensor();
+
+      // Act
+      publisher.start();
+
+      // Assert
+      assert(publisher.sensor.addEventListener.calledWith('reading'));
+      assert(publisher.sensor.start.called);
+    });
+  });
+  // Test stop function
+  describe('#stop()', function() {
+    it('should stop and remove event listener', function() {
+      // Arrange
+      const ros = new ROSLIB.Ros();
+      const publisher = new AmbientLightPublisher(ros, 'topic');
+      publisher.sensor = spyOnLightSensor();
+
+      // Act
+      publisher.start();
+      publisher.stop();
+
+      // Assert
+      assert(publisher.sensor.removeEventListener.calledWith('reading'));
+      assert(publisher.sensor.stop.called);
+    });
+  });
+
+  // Test snapshot functionality
+  describe('#createSnapshot()', function() {
+    it('shoud capture the ambient light', function() {
+      // Arrange
+      const ros = new ROSLIB.Ros();
+      const publisher = new AmbientLightPublisher(ros, 'topic');
+      publisher.sensor = spyOnLightSensor();
+      publisher.light = 10;  
+      publisher.topic.publish = sinon.spy();
+
+      // Act
+      publisher.createSnapshot();
+
+      // Arrange
+      const sentMsg = publisher.topic.publish.args[0][0];
+      assert.equal(sentMsg.data, 10);
     });
   });
 });
